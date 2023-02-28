81a35e4fd8500bec2cc2f8ebda456d71326152ed
SMpofana <53563133+SMpofana@users.noreply.github.com>
2023-02-28 14:49:50 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//navigation
+//Main menu navigation
 
 const burgerMenu = document.querySelector(".hamburger_lines")
 const burgerline1 = burgerMenu.firstElementChild;
@@ -98,7 +98,164 @@ for(let i=0; i<workTabHeaderNodes.length; i++){
 
   
 }
-//Our work automation
+
+//Our work nav
+//Business work
+
+const busTrack = document.querySelector(".business__work__container");
+const busCards = Array.from(busTrack.children);
+const busNav = document.querySelector(".business_work_nav");
+const busNextButton = busNav.lastElementChild;
+const busPrevButton = busNav.firstElementChild;
+
+
+
+const busSlideWidth = busCards[0].getBoundingClientRect().width;
+
+
+
+
+const setBusCardPosition = (card, index) =>{
+  card.style.left = busSlideWidth*index + 'px'
+}
+
+busCards.forEach(setBusCardPosition)
+
+
+
+const moveToSlide = (track, currentSlide, targetSlide) =>{
+  track.style.transform = 'translateX(-'+ targetSlide.style.left +')';
+  currentSlide.classList.remove("current_card");
+  targetSlide.classList.add("current_card");
+  
+}
+
+//Business click on right button
+
+busNextButton.addEventListener('click', e=>{
+  const currentBusCard = busTrack.querySelector(".current_card");
+  const nextBusCard = currentBusCard.nextElementSibling;
+
+  
+  moveToSlide(busTrack, currentBusCard, nextBusCard)
+
+});
+
+//Business click on left button
+busPrevButton.addEventListener('click', e=>{
+  const currentBusCard = busTrack.querySelector(".current_card")
+  const prevBusCard = currentBusCard.previousElementSibling
+
+  moveToSlide(busTrack, currentBusCard, prevBusCard)
+
+})
+
+
+//Homes work
+const homeTrack = document.querySelector(".home__work__container");
+const homeCards = Array.from(homeTrack.children)
+
+const homeNav = document.querySelector(".home_work_nav")
+const homeNextButton = homeNav.lastElementChild
+const homePrevButton = homeNav.firstElementChild
+
+const homeSlideWidth = homeCards[0].getBoundingClientRect().width;
+
+
+
+
+const setHomeCardPosition = (card, index) =>{
+  card.style.left = homeSlideWidth*index + 'px'
+}
+
+homeCards.forEach(setHomeCardPosition)
+
+
+
+
+//Home click on right button
+
+homeNextButton.addEventListener('click', e=>{
+  const currentHomeCard = homeTrack.querySelector(".current_card");
+  const nextHomeCard = currentHomeCard.nextElementSibling;
+
+  
+  moveToSlide(homeTrack, currentHomeCard, nextHomeCard)
+
+});
+
+//Home click on left button
+homePrevButton.addEventListener('click', e=>{
+  const currentHomeCard = homeTrack.querySelector(".current_card")
+  const prevHomeCard = currentHomeCard.previousElementSibling
+
+  moveToSlide(homeTrack, currentHomeCard, prevHomeCard)
+
+})
+
+//Solutions
+
+const busSol = document.getElementById("business_solutions")
+const busSolTrack = busSol.lastElementChild
+const busSolCards = Array.from(busSolTrack.children)
+
+const homeSol = document.getElementById("home_solutions")
+const homeSolTrack = homeSol.lastElementChild
+const homeSolCards = Array.from(homeSolTrack.children)
+
+
+let width = window.innerWidth
+
+
+// const observer = new IntersectionObserver(entries =>{
+
+// })
+
+// observer.observe()
+solutionAddActive = function(solution){solution.classList.add('active')};
+solutionRemoveActive = function(solution){solution.classList.remove('active')};
+
+
+if(width < 560){
+  console.log(width)
+  setInterval(()=>{
+    for(var i = 0; i < busSolCards.length; i++){
+      if(i+1 == busSolCards.length){
+        solutionAddActive(busSolCards[0]);
+        busSolCards[0].style.zIndex = 100;
+        setTimeout(solutionRemoveActive, 350, busSolCards[i]);
+        break;
+      }
+      if(busSolCards[i].classList.contains('active')){
+        busSolCards[i].removeAttribute('style');
+        setTimeout(solutionRemoveActive, 350, busSolCards[i]);
+        solutionAddActive(busSolCards[i + 1]);
+        break;
+      }
+    }
+    
+  }, 8000);
+
+  setInterval(()=>{
+    for(var i = 0; i < homeSolCards.length; i++){
+      if(i+1 == homeSolCards.length){
+        solutionAddActive(homeSolCards[0]);
+        homeSolCards[0].style.zIndex = 100;
+        setTimeout(solutionRemoveActive, 350, homeSolCards[i]);
+        break;
+      }
+      if(homeSolCards[i].classList.contains('active')){
+        homeSolCards[i].removeAttribute('style');
+        setTimeout(solutionRemoveActive, 350, homeSolCards[i]);
+        solutionAddActive(homeSolCards[i + 1]);
+        break;
+      }
+    }
+    
+  }, 8000);
+}
+
+
 
 
 
